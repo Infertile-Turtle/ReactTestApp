e807aef14eda318e202b1e8f68b1e05763630556
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-04-13 11:46:51 -0500

    "next"

--- src/App.js
+++ src/App.js
@@ -19,6 +19,46 @@
 
 // export default App;
 
+// import React, { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   const [generatedLink, setGeneratedLink] = useState(null);
+
+//   async function generateLink() {
+//     try {
+//       const response = await fetch(
+//         // 'https://8ich8omu47.execute-api.us-east-1.amazonaws.com/dev/test'
+//         'https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/fetchdata'
+//       );
+//       const data = await response.json();
+//       console.log(data);
+//       setGeneratedLink(data);
+//     } catch (err) {
+//       console.log(err);
+//       setGeneratedLink('Error retrieving data.');
+//     }
+//   }
+
+//   return (
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img
+//           src={logo}
+//           className='App-logo'
+//           alt='logo'
+//         />
+//         <h1>Hello from V2</h1>
+//         <button onClick={generateLink}>Generate Link</button>
+//         {generatedLink && <p>Generated Link: {generatedLink}</p>}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -26,19 +66,22 @@ import './App.css';
 function App() {
   const [generatedLink, setGeneratedLink] = useState(null);
 
-  async function generateLink() {
-    try {
-      const response = await fetch(
-        'https://8ich8omu47.execute-api.us-east-1.amazonaws.com/dev/test'
-      );
-      const data = await response.json();
-      console.log(data);
-      setGeneratedLink(data);
-    } catch (err) {
-      console.log(err);
-      setGeneratedLink('Error retrieving data.');
-    }
-  }
+  const generateLink = async () => {
+    const response = await fetch(
+      'https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/generateurl',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Clicks: 1,
+        }),
+      }
+    );
+    const data = await response.json();
+    setGeneratedLink(data.link);
+  };
 
   return (
     <div className='App'>
@@ -48,7 +91,7 @@ function App() {
           className='App-logo'
           alt='logo'
         />
-        <h1>Hello from V4</h1>
+        <h1>Hello from V1</h1>
         <button onClick={generateLink}>Generate Link</button>
         {generatedLink && <p>Generated Link: {generatedLink}</p>}
       </header>

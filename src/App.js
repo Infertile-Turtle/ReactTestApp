0d9da9f387ba8a42b4f09ba5635e91eb00f4ca8d
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-04-14 10:42:28 -0500

    push

--- src/App.js
+++ src/App.js
@@ -59,29 +59,81 @@
 
 // export default App;
 
+// import React, { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   const [generatedLink, setGeneratedLink] = useState(null);
+
+//   const generateLink = async () => {
+//     const response = await fetch(
+//       'https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/generateurl',
+//       {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           Clicks: 1,
+//         }),
+//       }
+//     );
+//     const data = await response.json();
+//     setGeneratedLink(data.link);
+//   };
+
+//   return (
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img
+//           src={logo}
+//           className='App-logo'
+//           alt='logo'
+//         />
+//         <h1>Hello from V1</h1>
+//         <button onClick={generateLink}>Generate Link</button>
+//         {generatedLink && <p>Generated Link: {generatedLink}</p>}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [generatedLink, setGeneratedLink] = useState(null);
+  const [linkId, setLinkId] = useState('');
 
-  const generateLink = async () => {
-    const response = await fetch(
-      'https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/generateurl',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          Clicks: 1,
-        }),
-      }
-    );
-    const data = await response.json();
-    setGeneratedLink(data.link);
-  };
+  // function to get linkId from the URL
+  function getLinkIdFromUrl() {
+    const url = window.location.href;
+    const linkId = url.substr(url.lastIndexOf('/') + 1);
+    setLinkId(linkId);
+  }
+
+  // function to call the API and update click count
+  async function handleClick() {
+    const url = `https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/fetchdata`;
+    const data = { linkID: linkId };
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    console.log(result);
+
+    const clickUrl = `https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/updateclickcount`;
+    await fetch(clickUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+  }
 
   return (
     <div className='App'>
@@ -91,9 +143,8 @@ function App() {
           className='App-logo'
           alt='logo'
         />
-        <h1>Hello from V1</h1>
-        <button onClick={generateLink}>Generate Link</button>
-        {generatedLink && <p>Generated Link: {generatedLink}</p>}
+        <h1>Hello from V2</h1>
+        <button onClick={handleClick}>Generate Link</button>
       </header>
     </div>
   );

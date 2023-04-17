cbdb48e1d49ce73ed9ce37ea0024790a05b00428
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-04-17 11:50:08 -0500

    almost

--- src/App.js
+++ src/App.js
@@ -98,35 +98,20 @@ function App() {
       body: JSON.stringify(data),
     });
 
-    console.log('result1', result);
-    console.log('response1', response);
-
     if (!response.ok) {
-      console.log('response NOT OK block running');
-      console.log('result2', result);
-      console.log('response2', response);
       setError(true);
       setResult(null);
-      console.log('result3', result);
-      console.log('response3', response);
       return;
     }
 
     const responseData = await response.json();
     if (responseData.errorType) {
-      console.log('API Error:', responseData.errorMessage);
-      console.log('result4', result);
       setError(true);
       setResult(null);
-      console.log('result5', result);
       return;
     }
 
-    console.log('result6', result);
-    console.log('response6', response);
     setResult(responseData);
-    console.log('result7', result);
-    console.log('response7', response);
 
     const clickUrl = `https://fg4vvveib0.execute-api.us-east-1.amazonaws.com/dev/updateclickcount`;
     await fetch(clickUrl, {
@@ -134,11 +119,11 @@ function App() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
     });
-
-    console.log('result8', result);
-    console.log('response8', response);
-    console.log('data8', data);
   }
+
+  useEffect(() => {
+    console.log('result updated:', result);
+  }, [result]);
 
   return (
     <div className='App'>
